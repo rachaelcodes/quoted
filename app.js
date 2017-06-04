334f6b25cc0e49a016872a03d7c7f1f74ac04ca2
rachaelcodes <rachaelcodes@users.noreply.github.com>
2017-06-04 13:53:42 +0100

    Updated with new quote JSON, a randomizer and styling options.

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-//https://theysaidso.com/api/
-$(document).ready(function(){
-	$("#box").on("click", function(){
-		$("#quote").html("http://quotes.rest/qod.json?category=art");
-	});
-});
+document.getElementById("box").addEventListener("click", function(){
+	document.getElementById("instructions").style.display = "none";
+
+	var a = Math.floor(Math.random() * 74);
+	var b = data[a]["words"];
+	var c = "\x2D " + data[a]["speaker"];
+
+	if (document.getElementById("quote").lastChild){
+		document.getElementById("quote").removeChild(document.getElementById("quote").lastChild);
+	}
+	if (document.getElementById("quoted").lastChild){
+		document.getElementById("quoted").removeChild(document.getElementById("quoted").lastChild);
+	}
+	document.getElementById("quote").appendChild(document.createTextNode(b));
+	document.getElementById("quoted").appendChild(document.createTextNode(c));
+
+	document.getElementById('frame').src = 'iframe.html';
+}, false);
---
+++ quotes.js
@@ -0,0 +1,378 @@
+var data = [
+  {
+    "ref": 1,
+    "words": "Art is magic delivered from the lie of being truth.",
+    "speaker": "Theodor Adorno",
+  },
+  {
+    "ref": 2,
+    "words": "Any great work of art ... revives and readapts time and space, and the measure of its success is the extent to which it makes you an inhabitant of that world \x2D the extent to which it invites you in and lets you breathe its strange, special air.",
+    "speaker": "Leonard Bernstein",
+  },
+  {
+    "ref": 3,
+    "words": "Degrade first the arts if you\'d mankind degrade, \n Hire idiots to paint with cold light and hot shade.",
+    "speaker": "William Blake",
+  },
+  {
+    "ref": 4,
+    "words": "A writer \x2D and, I believe, generally all persons \x2D must think that whatever happens to him or her is a resource. All things have been given to us for a purpose, and an artist must feel this more intensely. All that happens to us, including our humiliations, our misfortunes, our embarrassments, all is given to us as raw material, as clay, so that we may shape our art.",
+    "speaker": "Jorge Luis Borges",
+  },
+  {
+    "ref": 5,
+    "words": "While our art cannot, as we wish it could, save us from wars, privation, envy, greed, old age, or death, it can revitalize us amidst it all.",
+    "speaker": "Ray Bradbury",
+  },
+  {
+    "ref": 6,
+    "words": "Art today can only be revolutionary, that is, it must aspire at the complete and radical reconstruction of society, even if for no other reason than to emancipate intellectual creation from the chains which obstruct it and to allow all mankind to rise to the heights that only geniuses could reach in the past.",
+    "speaker": "Andr\xE9 Breton",
+  },
+  {
+    "ref": 7,
+    "words": "What is art \n But life upon the larger scale, the higher,",
+    "speaker": "Elizabeth Barrett Browning",
+  },
+  {
+    "ref": 8,
+    "words": "Art\'s life \x2D and where we live, we suffer and toil.",
+    "speaker": "Elizabeth Barrett Browning",
+  },
+  {
+    "ref": 9,
+    "words": "If the world were clear, art would not exist.",
+    "speaker": "Albert Camus",
+  },
+  {
+    "ref": 10,
+    "words": "Art is the triumph over chaos.",
+    "speaker": "John Cheever",
+  },
+  {
+    "ref": 11,
+    "words": "Art, like morality, consists in drawing the line somewhere.",
+    "speaker": "G.K. Chesterton",
+  },
+  {
+    "ref": 12,
+    "words": "Without tradition, art is a flock of sheep without a shepherd. Without innovation, it is a corpse.",
+    "speaker": "Winston Churchill",
+  },
+  {
+    "ref": 13,
+    "words": "All creative art is magic, is evocation of the unseen in forms persuasive, enlightening, familiar and surprising, for the edification of mankind, pinned down by the conditions of its existence to the earnest consideration of the most insignificant tides of reality.",
+    "speaker": "Joseph Conrad",
+  },
+  {
+    "ref": 14,
+    "words": "I do a bale of sketches, one eye, a piece of hair. A pound of observation, then an ounce of painting.",
+    "speaker": "Gardner Cox",
+  },
+  {
+    "ref": 15,
+    "words": "The desire to be loved is really death when it comes to art.",
+    "speaker": "David Cronenberg",
+  },
+  {
+    "ref": 16,
+    "words": "Drawing is the honesty of the art. There is no possibility of cheating. It is either good or bad.",
+    "speaker": "Salvador Dal\xED",
+  },
+  {
+    "ref": 17,
+    "words": "Art, as far as it is able, follows nature, as a pupil imitates his master\x3B thus your art must be, as it were, God\'s grandchild.",
+    "speaker": "Dante Alighieri",
+  },
+  {
+    "ref": 18,
+    "words": "Art is not the possession of the few who are recognized writers, painters, musicians\x3B it is the authentic expression of any and all individuality.",
+    "speaker": "John Dewey",
+  },
+  {
+    "ref": 19,
+    "words": "All passes, Art alone \n Enduring stays to us",
+    "speaker": "Austin Dobson",
+  },
+  {
+    "ref": 20,
+    "words": "Immature poets imitate\x3B mature poets steal.",
+    "speaker": "T.S. Eliot",
+  },
+  {
+    "ref": 21,
+    "words": "Rationality is what we do to organize the world, to make it possible to predict. Art is the rehearsal for the inapplicability and failure of that process.",
+    "speaker": "Brian Eno",
+  },
+  {
+    "ref": 22,
+    "words": "Where humans must face the constant threat of starvation, where life is reduced to the bare essentials \x2D it turns out that one of these essentials is art",
+    "speaker": "Peter Farb",
+  },
+  {
+    "ref": 23,
+    "words": "All art is autobiographical\x3B the pearl is the oyster\'s autobiography.",
+    "speaker": "Federico Fellini",
+  },
+  {
+    "ref": 24,
+    "words": "A master in art need not go into the highways and byways for affects\x3B he knows the straight course and follows it",
+    "speaker": "Ernest Flagg",
+  },
+  {
+    "ref": 25,
+    "words": "You can calculate the worth of a man by the number of his enemies, and the importance of a work of art by the harm that is spoken of it.",
+    "speaker": "Gustave Flaubert",
+  },
+  {
+    "ref": 26,
+    "words": "Great artists are people who find the way to be themselves in their art. Any sort of pretension induces mediocrity in art and life alike.",
+    "speaker": "Margot Fonteyn",
+  },
+  {
+    "ref": 27,
+    "words": "\'Collective art,\'... is not an individual \'leisure time\' occupation, added to life, it is an integral part of life.",
+    "speaker": "Erich Fromm",
+  },
+  {
+    "ref": 28,
+    "words": "I don\'t consider myself an artist. I go out there and I try to play what\'s right for the music.",
+    "speaker": "Steve Gadd",
+  },
+  {
+    "ref": 29,
+    "words": "Art attracts us only by what it reveals of our most secret self.",
+    "speaker": "Jean-Luc Godard",
+  },
+  {
+    "ref": 30,
+    "words": "I dream my painting, and then I paint my dream.",
+    "speaker": "Vincent van Gogh",
+  },
+  {
+    "ref": 31,
+    "words": "Art quickens nature\x3B care will make a face\x3B Neglected beauty perisheth apace.",
+    "speaker": "Robert Herrick",
+  },
+  {
+    "ref": 32,
+    "words": "Ars longa, vita brevis est.",
+    "speaker": "Hippocrates",
+  },
+  {
+    "ref": 33,
+    "words": "For art to be art it has to cure.",
+    "speaker": "Alejandro Jodorowsky",
+  },
+  {
+    "ref": 34,
+    "words": "Art hath an enemy called Ignorance.",
+    "speaker": "Ben Jonson",
+  },
+  {
+    "ref": 35,
+    "words": "Art is the human disposition of sensible or intelligible matter for an aesthetic end.",
+    "speaker": "James Joyce",
+  },
+  {
+    "ref": 36,
+    "words": "The function of the modern artist was not to convey beauty, but to convey new truths.",
+    "speaker": "Eric Kandel",
+  },
+  {
+    "ref": 37,
+    "words": "We must never forget that art is not a form of propaganda\x3B it is a form of truth.",
+    "speaker": "John F. Kennedy",
+  },
+  {
+    "ref": 38,
+    "words": "Creation always involves building upon something else. There is no art that doesn\'t reuse.",
+    "speaker": "Lawrence Lessig",
+  },
+  {
+    "ref": 39,
+    "words": "Bad art is more tragically beautiful than good art, because it documents human failure.",
+    "speaker": "Henry Letham",
+  },
+  {
+    "ref": 40,
+    "words": "All art is solitary and the studio is a torture area.",
+    "speaker": "Alexander Liberman",
+  },
+  {
+    "ref": 41,
+    "words": "A genuine work of art must mean many things\x3B the truer its art, the more things it will mean.",
+    "speaker": "George MacDonald",
+  },
+  {
+    "ref": 42,
+    "words": "It is there not so much to convey a meaning as to wake a meaning. If it do not even wake an interest, throw it aside. A meaning may be there, but it is not for you.",
+    "speaker": "George MacDonald",
+  },
+  {
+    "ref": 43,
+    "words": "Art, in fact, can be nothing but violence, cruelty and injustice.",
+    "speaker": "Filippo Tommaso Marinetti",
+  },
+  {
+    "ref": 44,
+    "words": "Drawing is like making an expressive gesture with the advantage of permanence.",
+    "speaker": "Henri Matisse",
+  },
+  {
+    "ref": 45,
+    "words": "Art does not imitate, but interpret.",
+    "speaker": "Giuseppe Mazzini",
+  },
+  {
+    "ref": 46,
+    "words": "I don\'t distinguish between magic and art.",
+    "speaker": "Alan Moore",
+  },
+  {
+    "ref": 47,
+    "words": "Most painting in the European tradition was painting the mask. Modern art rejected all that. Our subject matter was the person behind the mask.",
+    "speaker": "Robert Motherwell",
+  },
+  {
+    "ref": 48,
+    "words": "Many are willing to suffer for their art. Few are willing to learn to draw.",
+    "speaker": "Simon Munnery",
+  },
+  {
+    "ref": 49,
+    "words": "Art is the supreme task and the truly metaphysical activity in this life...",
+    "speaker": "Friedrich Nietzsche",
+  },
+  {
+    "ref": 50,
+    "words": "Art is marks on canvas trying to find a place to live.",
+    "speaker": "Bill O\'Leary",
+  },
+  {
+    "ref": 51,
+    "words": "Were art to redeem man, it could do so only by saving him from the seriousness of life and restoring him to an unexpected boyishness.",
+    "speaker": "Jos\xE9 Ortega y Gasset",
+  },
+  {
+    "ref": 52,
+    "words": "By arts, sails, and oars, ships are rapidly moved; arts move the light chariot, and establish love.",
+    "speaker": "Ovid",
+  },
+  {
+    "ref": 53,
+    "words": "Art has two constant, two unending concerns\x3A It always meditates on death and thus always creates life.",
+    "speaker": "Boris Pasternak",
+  },
+  {
+    "ref": 54,
+    "words": "Art and music make manifest, by bringing into conscious awareness, that which has previously been felt only tentatively and internally.",
+    "speaker": "F. David Peat",
+  },
+  {
+    "ref": 55,
+    "words": "For a long time I limited myself to one color \x2D as a form of discipline.",
+    "speaker": "Pablo Picasso",
+  },
+  {
+    "ref": 56,
+    "words": "Abstract painting is abstract. It confronts you.",
+    "speaker": "Jackson Pollock",
+  },
+  {
+    "ref": 57,
+    "words": "The perfection of art is to conceal art.",
+    "speaker": "Quintilian",
+  },
+  {
+    "ref": 58,
+    "words": "The work of art ... is an instrument for tilling the human psyche, that it may continue to yield a harvest of vital beauty.",
+    "speaker": "Herbert Read",
+  },
+  {
+    "ref": 59,
+    "words": "Art is indeed not the bread but the wine of life.",
+    "speaker": "Jean Paul Richter",
+  },
+  {
+    "ref": 60,
+    "words": "Life without industry is guilt, and industry without art is brutality.",
+    "speaker": "John Ruskin",
+  },
+  {
+    "ref": 61,
+    "words": "Greater completion marks the progress of art, absolute completion usually its decline.",
+    "speaker": "John Ruskin",
+  },
+  {
+    "ref": 62,
+    "words": "Art is what is irresistible.",
+    "speaker": "William Saroyan",
+  },
+  {
+    "ref": 63,
+    "words": "The role of art is to make a world which can be inhabited.",
+    "speaker": "William Saroyan",
+  },
+  {
+    "ref": 64,
+    "words": "Art is the right hand of Nature. The latter has only given us being, the former has made us men.",
+    "speaker": "Friedrich Schiller",
+  },
+  {
+    "ref": 65,
+    "words": "In the largest sense, every work of art is protest.",
+    "speaker": "Pete Seeger",
+  },
+  {
+    "ref": 66,
+    "words": "Art is the magic mirror you make to reflect your invisible dreams in visible pictures. You use a glass mirror to see your face\x3A you use works of art to see your soul.",
+    "speaker": "George Bernard Shaw",
+  },
+  {
+    "ref": 67,
+    "words": "There is no pulse so sure of the state of a nation as its characteristic art product which has nothing to do with its material life.",
+    "speaker": "Gertrude Stein",
+  },
+  {
+    "ref": 68,
+    "words": "We create art to express our perception as to what is not visible in nature. There is no past or future in art.... art should always be in the present\x21",
+    "speaker": "Marko Stout",
+  },
+  {
+    "ref": 69,
+    "words": "Art is a human activity having for its purpose the transmission to others of the highest and best feelings to which men have risen.",
+    "speaker": "Leo Tolstoy",
+  },
+  {
+    "ref": 70,
+    "words": "Art happens all the time, everywhere. All we have to do is to keep our minds open.",
+    "speaker": "Jacek Tylicki",
+  },
+  {
+    "ref": 71,
+    "words": "Art is the symbol of the two noblest human efforts\x3A to construct and to refrain from destruction.",
+    "speaker": "Simone Weil",
+  },
+  {
+    "ref": 72,
+    "words": "We can forgive a man for making a useful thing, as long as he does not admire it. The only excuse for making a useless thing is that one admires it intensely. All art is quite useless.",
+    "speaker": "Oscar Wilde",
+  },
+  {
+    "ref": 73,
+    "words": "Art is individualism, and individualism is a disturbing and disintegrating force. There lies its immense value. For what it seeks is to disturb monotony of type, slavery of custom, tyranny of habit, and the reduction of man to the level of a machine.",
+    "speaker": "Oscar Wilde",
+  },
+  {
+    "ref": 74,
+    "words": "In short, if newspapers were written by people whose sole object in writing was to tell the truth about politics and the truth about art we should not believe in war, and we should believe in art.",
+    "speaker": "Virginia Woolf",
+  },
+  {
+    "ref": 75,
+    "words": "The activity of art is... as important as the activity of language itself, and as universal.",
+    "speaker": "Leo Tolstoy",
+  },
+
+]
