38e13586a0eb41f07101635ed4d90767f6f05163
rachaelcodes <rachaelcodes@users.noreply.github.com>
2017-06-07 21:38:32 +0100

    fixed the tweet button  Please enter the commit message for your changes. Lines starting

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.getElementById("box").addEventListener("click", function(){
 	var a = Math.floor(Math.random() * 74);
 	var b = data[a]["words"];
 	var c = "\x2D " + data[a]["speaker"];
+	var tweetdiv = document.getElementById('twtbox');
 
 	if (document.getElementById("quote").lastChild){
 		document.getElementById("quote").removeChild(document.getElementById("quote").lastChild);
@@ -15,5 +16,23 @@ document.getElementById("box").addEventListener("click", function(){
 	document.getElementById("quoted").appendChild(document.createTextNode(c));
 
 	document.getElementById('frame').src = 'iframe.html';
-	document.getElementsByClassName('twitter-share-button')[0].dataText=b+" "+c;
+
+	var myNode = document.getElementById('twtbox');
+  while (myNode.firstChild) {
+    myNode.removeChild(myNode.firstChild);
+  }
+
+	var link = document.createElement('a');
+	link.setAttribute('href', 'https://twitter.com/share');
+	link.setAttribute('class', 'twitter-share-button');
+	link.setAttribute('id', 'twitterbutton');
+	link.setAttribute('style', 'margin-top:5px;');
+	link.setAttribute('data-text', b + " " + c);
+	link.setAttribute('data-via', 'rachaelcodes');
+	link.setAttribute('data-size', 'large');
+
+
+	tweetdiv.appendChild(link);
+
+	twttr.widgets.load();
 }, false);
